e508c488cb4b76db3a55bd551e738b667ef26d03
sachin <sachin@DESKTOP-T44FEO9>
2022-12-21 10:33:21 +0530

    my first commit

---
+++ MyApplication/src/example/git/DateClass.java
@@ -0,0 +1,14 @@
+package example.git;
+
+import java.util.Date;
+
+public class DateClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("hello world");
+     Date d=new Date();
+     System.out.println(d);
+	}
+
+}
